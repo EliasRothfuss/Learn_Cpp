d8e0b08b3109b15274296ee81ca5559a1fb08d2b
Elias Rothfuss <eliasrothfuss@gmail.com>
2023-04-28 11:24:20 +0200

    updated New_Project.sh

--- New_Project.sh
+++ New_Project.sh
@@ -1,26 +1,16 @@
 #!/bin/bash
-echo Project Name
+echo Project Name?
 read ProjectName
 
 mkdir $ProjectName
 cd $ProjectName
-
-cat << EOF > $ProjectName.c++
-#include <iostream>
-#include <stdint.h>
-
-auto main() -> int
-{
-    std::cout << "Project $ProjectName" << std::endl;
-}
-EOF
 
 cat << EOF > CMakeLists.txt
 # define the minimum cmake version
 cmake_minimum_required(VERSION 3.20)
 
 # define the project name
-project(Hello_World VERSION 1.0 LANGUAGES CXX)
+project($ProjectName VERSION 1.0 LANGUAGES CXX)
 
 # define the c standard version
 set(CMAKE_CXX_STANDARD 14)
@@ -30,7 +20,27 @@ set(CMAKE_CXX_STANDARD_REQUIRED True)
 add_compile_options(-Wall -Wextra -Werror -pedantic -O0)
 
 # add an executabel and assign files to it
-add_executable(exec_$ProjectName $ProjectName.c++)
+add_executable(main_exec src/main.c++)
 EOF
+
+mkdir include
+mkdir test
+
+mkdir src
+cd src
+
+cat << EOF > main.c++
+#include <iostream>
+#include <stdint.h>
+
+auto main() -> int
+{
+    std::cout << "Project $ProjectName" << std::endl;
+}
+EOF
+
+cd .. 
+
+cp ../.gitignore .
 
 code .
