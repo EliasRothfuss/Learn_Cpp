e453e2bfe9b0b41bb5127108316865fcd689bb54
Elias Rothfuss <eliasrothfuss@gmail.com>
2023-05-05 20:18:06 +0200

    Added Libs and Tests to New_Project.sh

--- New_Project.sh
+++ New_Project.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 echo Project Name?
 read ProjectName
+echo Random_String?
+read Random_String
 
 mkdir $ProjectName
 cd $ProjectName
@@ -19,27 +21,91 @@ set(CMAKE_CXX_STANDARD_REQUIRED True)
 # add some compile options
 add_compile_options(-Wall -Wextra -Werror -pedantic -O0)
 
+# add a library for your student structure
+add_library(${ProjectName}_exec src/$ProjectName.cpp)
+# add the includes to the library
+target_include_directories(${ProjectName}_exec PUBLIC include)
+
 # add an executabel and assign files to it
-add_executable(main_exec src/main.c++)
+add_executable(main_exec src/main.cpp)
+
+target_include_directories(main_exec PRIVATE include)
+# link your libraries
+target_link_libraries(main_exec PRIVATE ${ProjectName}_exec)
+
+# check if we have gtest
+find_package(GTest)
+if(GTEST_LIBRARY)
+    enable_testing()
+    # include the gtest CMake stuff
+    include(GoogleTest)
+    # create a test executable
+    add_executable(tests test/${ProjectName}_test.cpp)
+    # link the gtest libraries
+    target_link_libraries(tests PRIVATE GTest::GTest GTest::Main)
+    # link the library we want to test
+    target_link_libraries(tests PRIVATE ${ProjectName}_exec)
+    # add the include directories
+    target_include_directories(tests PUBLIC include)
+    # discover and add tests to the test list
+    gtest_discover_tests(tests)
+endif()
+
 EOF
 
 mkdir include
-mkdir test
+cd include
+
+cat << EOF > $ProjectName.h
+#ifndef ${ProjectName^^}_${Random_String^^}
+#define ${ProjectName^^}_${Random_String^^}
+
+void say_hi();
+
+#endif
+
+EOF
+cd ..
 
 mkdir src
 cd src
 
-cat << EOF > main.c++
+cat << EOF > main.cpp
 #include <iostream>
 #include <stdint.h>
+#include "$ProjectName.h"
 
 auto main() -> int
 {
-    std::cout << "Project $ProjectName" << std::endl;
+    say_hi();
+
+    return 0;
 }
+
 EOF
 
+cat << EOF > $ProjectName.cpp
+#include <iostream>
+#include <stdint.h>
+#include "$ProjectName.h"
+
+void say_hi()
+{
+    std::cout << "Project $ProjectName" << std::endl;
+}
+
+EOF
 cd .. 
+
+mkdir test
+cd test
+
+cat << EOF > ${ProjectName}_test.cpp
+#include "gtest/gtest.h"  // include the gtest functions & macros
+#include "$ProjectName.h"
+
+EOF
+cd ..
 
 cp ../.gitignore .
 
